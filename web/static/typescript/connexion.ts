0c1bf949e57a13fa878b5dda817210dea3f10ee5
BuildTools <unconfigured@null.spigotmc.org>
2025-09-16 13:55:26 +0200

    first commit

---
+++ web/static/typescript/connexion.ts
@@ -0,0 +1,4 @@
+declare let urlApi;
+
+let loginInput:HTMLInputElement = <HTMLInputElement>document.getElementById("loginInput");
+let passwordInput:HTMLInputElement = <HTMLInputElement>document.getElementById("passwordInput");
